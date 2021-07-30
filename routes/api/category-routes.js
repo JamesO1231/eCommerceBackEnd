cdf74859e9319ab8841899e1a1572f65b86317ec
James Odell <jpodell21@gmail.com>
2021-07-30 13:55:22 -0600

    finished API/routes and made changes to seeds/server.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,15 +43,49 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    categoryName: req.body.categoryName,
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData[0]) {
+      res.status(404).json({ message: 'No category found under this ID'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No category found under this ID'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
